f1779f04a886f6a5b353ff69820524af8d77e680
vivek seelam <vivek221b@gmail.com>
2022-12-15 17:48:16 +0530

    added users app and added urls to test

---
+++ users/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    message = f'{request.GET["cool_boy"]}'
+    if message == 'vivek':
+        message = "vvievk is the best"
+    return HttpResponse(f"THis is wierd {message}")
+
