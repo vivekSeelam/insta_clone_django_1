2397f372ef33b13bb871a4cac66e5c5f039190d1
vivek seelam <vivek221b@gmail.com>
2022-12-16 11:37:09 +0530

    models intro, lec 4

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=256, null=False)
+    email = models.EmailField(max_length=256, null=False, unique=True)
+    phone_num = models.CharField(max_length=256, null=False, unique=True)
+    is_active = models.BooleanField(default=False)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class UserProfile(models.Model):
+    DEFAULT_PROFILE_PIC_URl = "https://mywebsite.com/placeholder.png"
+
+    profile_pic_url = models.CharField(max_length=256, default=DEFAULT_PROFILE_PIC_URl)
+    bio = models.CharField(max_length=256, blank=True)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
